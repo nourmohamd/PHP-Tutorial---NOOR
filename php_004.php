63c95ec140953fe4ca1cb4a142eb0977903c1b73
Mohamed Nour Abdo <abdonoor684@gmail.com>
2025-03-14 22:49:24 +0300

    Upload Modification And Create A New File PHP_005

--- php_004.php
+++ php_004.php
@@ -21,4 +21,28 @@ $_SERVER["SERVER_ADDR"] ===> Number Of IP For This Device
 $_SERVER["REMOTE_ADDR"] ===> Number Of IP For Place You Use Internet in it
 */
 
-?>
+// Lesson 24
+// Encode IN PHP
+// md5(Data) ===> For Encode
+// sha1(Data) ===> For Encode But Stronger Than md5
+// Example:
+$text = "M";
+$encode1 = md5($text);
+$encode2 = sha1($text);
+echo $text;
+echo $encode1;
+echo $encode2;
+
+// Lesson 25
+// New Method
+/*
+1 - explode("Separetor", String); ===> Convertation String To Array
+2 - implode($array) ===> Convertation Array To String
+3 - strcmp(Str1, Str2) ===> For Compare Between Two Strings
+*/
+
+// Lesson 26
+// Composer ===> Tool For Include Libraries in PHP Language
+// Download Composer And Download XAMPP 7.4.1 for <<Compatibility>>
+    // Open CMD ===> Write ===> composer require nameOfLaibrary
+    ?>
---
+++ php_005.php
@@ -0,0 +1,9 @@
+<?php
+// Lesson 27
+// mPDF is a PHP library which generates PDF files from UTF-8 encoded HTML.
+// https://github.com/mpdf/mpdf ===> URL For MPdf Library
+// PHP 8.0 is supported since mPDF v8.0.10
+// Installation : 
+// cmd ===> Select Directory ===> composer require mpdf/mpdf v8.0.10
+
+?>
